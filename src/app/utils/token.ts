b418598774443bf08c0a2ba2dbb8176e9cd6f5d3
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-20 20:17:23 +0530

    utils added

---
+++ src/app/utils/token.ts
@@ -0,0 +1,14 @@
+
+
+
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userId: string) => {
+ try {
+    
+ } catch (error) {
+    console.error('Error generating token:', error);
+    throw new Error('Token generation failed');
+  }
+    
+ }
