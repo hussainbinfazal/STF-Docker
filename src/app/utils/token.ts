712a6f26586fe2813013bcb5b6ee6898cdc1d0a7
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-21 18:47:05 +0530

    token file re added

--- src/app/utils/token.ts
+++ src/app/utils/token.ts
@@ -1,14 +1,27 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { NextResponse } from "next/server";
+interface JWT extends JwtPayload {
+  userId: string;
+  roles: string[];
+}
+interface res extends NextResponse {
+  token?: string;
+}
+export const generateToken = (userId: string, response: res) => {
+  try {
+    let token: string;
+    if (!userId) return response;
 
+    const secretKey = process.env.NEXT_PUBLIC_JWT_SECRET_KEY;
 
+    if (!secretKey) {
+      throw Error("JWT secret key is not defined");
+    }
 
-const jwt = require('jsonwebtoken');
-
-const generateToken = (userId: string) => {
- try {
-    
- } catch (error) {
-    console.error('Error generating token:', error);
-    throw  Error('Token generation failed');
+    token = jwt.sign({ userId }, secretKey, { expiresIn: "1h" });
+    return token;
+  } catch (error) {
+    console.error("Error generating token:", error);
+    throw Error("Token generation failed");
   }
-    
- }
+};
