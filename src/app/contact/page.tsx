31e748d407c151d8c9b763616847ac9489031263
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-22 20:43:45 +0530

    contact page added

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page: React.FC = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+      <p>Thanks For contacting Smart Fiber Internet</p>
+      <p>we'll Contact you shortly</p>
+    </div>
+  )
+}
+
+export default page
