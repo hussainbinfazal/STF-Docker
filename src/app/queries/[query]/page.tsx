4b6908a6fa5ec09731fadb758f1a3b55649c5f86
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-23 03:21:13 +0530

    query page added

---
+++ src/app/queries/[query]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page: React.FC = () => {
+  return <div>
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+      <p>Please write your queries here</p>
+      <p>Please add your email here</p>
+        <p>We will get back to you shortly</p>
+    </div>
+  </div>;
+};
+
+export default page;
