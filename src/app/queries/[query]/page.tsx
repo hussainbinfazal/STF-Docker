a88642a73afc4cf017817d274aebf4ae672731f8
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-23 03:26:33 +0530

    submit function added

--- src/app/queries/[query]/page.tsx
+++ src/app/queries/[query]/page.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 
 const page: React.FC = () => {
-  return <div>
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
-      <p>Please write your queries here</p>
-      <p>Please add your email here</p>
-        <p>We will get back to you shortly</p>
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <p>Please write your queries here</p>
+        <input
+          type="text"
+          placeholder="Enter your query"
+          className="border border-gray-300 rounded-md p-2 mb-4"
+        />
+        <p>Please add your email here</p>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="border border-gray-300 rounded-md p-2 mb-4"
+        />
+        <p>Our team will get back to you shortly</p>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default page;
