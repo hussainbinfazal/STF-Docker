ccebf3a8b1411516d4d44d5dbc34245af3d662a0
hussainbinfazal <mdhussain0610@gmail.com>
2025-09-27 22:16:23 +0530

    added olt API controller

---
+++ src/app/api/olt/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET(req:NextResponse) {
+    connectDB()
+    const Firmware = await Firmware.find()
+    if(!Firmware) {
+        return NextResponse.json({message: "No Firmware found"}, {status: 404})
+    }
+    return NextResponse.json({Firmware}, {status: 200})
+}
