4c4f4544f066a444f00bb891d0f73f76802caf37
hussainbinfazal <mdhussain0610@gmail.com>
2025-09-30 00:28:48 +0530

    added change password route

---
+++ src/app/api/changepassword/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse, NextRequest } from "next/server";
+import connectDB from "@/app/utils/db";
+
+
+
+export async function POST(req: NextRequest) {
+    try {
+        connectDB();
+    const { email, password } = await req.json();
+    const user = await User.findOne({ email });
+    if (!user) {
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+    user.password = password;
+    await user.save();
+    return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+        
+    }
+}
