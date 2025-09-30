e041dc2e3200221c13717e0be5010233b4fc40be
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-01 04:19:27 +0530

    added update route

--- src/app/api/changepassword/route.ts
+++ src/app/api/changepassword/route.ts
@@ -19,3 +19,19 @@ export async function POST(req: NextRequest) {
         
     }
 }
+export async function PUT(req: NextRequest) {
+    try {
+        connectDB();
+    const { email, password } = await req.json();
+    const user = await User.findOne({ email });
+    if (!user) {
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+    user.password = password;
+    await user.save();
+    return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+        
+    }
+}
