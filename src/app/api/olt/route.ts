a11dd097932e8b88c786e79c4718c5b64cfdc9d0
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-04 23:27:13 +0530

    added delete route in the olt's api

--- src/app/api/olt/route.ts
+++ src/app/api/olt/route.ts
@@ -21,3 +21,13 @@ export async function PUT(req: NextRequest, { params }: { params: { OLTID: strin
     
     return NextResponse.json({OLTID, firmwareFile}, {status: 200})
 }
+
+export async function DELETE(req: NextRequest, { params }: { params: { OLTID: string }}) {
+    connectDB()
+    const { OLTID } = params
+    const firmware = await Firmware.findByIdAndDelete(OLTID)
+    if(!firmware) {
+        return NextResponse.json({message: "No Firmware found"}, {status: 404})
+    }
+    return NextResponse.json({OLTID}, {status: 200})
+    }
