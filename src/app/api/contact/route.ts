2b8f2e6641a2bf4cd301a5119f9a86a3360c2b55
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-05 22:38:04 +0530

    update controller added

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -10,3 +10,13 @@ export async function GET(req:NextResponse) {
     }
     return NextResponse.json({Firmware}, {status: 200})
 }
+
+export async function PUT(req:NextResponse) {
+    connectDB()
+    const Firmware = await Firmware.find()
+    
+    if(!Firmware) {
+        return NextResponse.json({message: "No Firmware found"}, {status: 404})
+    }
+    return NextResponse.json({Firmware}, {status: 200})
+}
