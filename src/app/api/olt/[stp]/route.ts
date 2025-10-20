4ad719e9d8670031ee68425b0eb2ad848aa904fc
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-20 22:58:51 +0530

    added stp Update Route

--- src/app/api/olt/[stp]/route.ts
+++ src/app/api/olt/[stp]/route.ts
@@ -8,3 +8,12 @@ export async function POST(req: NextRequest, { params }: { params: { stp: string
         return NextResponse.json({ message: error.message || "ERROR processing request" }, { status: 500 })
     }
 }
+export async function PUT(req: NextRequest, { params }: { params: { stp: string } }): Promise<NextResponse> {
+    try {
+        const { stp } = params
+        const stpInDb = await Firmware.findByIdAndUpdate(stp)
+        return NextResponse.json({ message: "Not implemented", stp }, { status: 501 })
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message || "ERROR processing request" }, { status: 500 })
+    }
+}
