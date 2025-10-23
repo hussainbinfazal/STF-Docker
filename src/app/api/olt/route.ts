3b702825bd45d6b698ddade6833adc265fcb59ef
hussainbinfazal <mdhussain0610@gmail.com>
2025-10-24 02:28:09 +0530

    added POST Stp Route

--- src/app/api/olt/route.ts
+++ src/app/api/olt/route.ts
@@ -8,6 +8,19 @@ export async function GET(req: NextRequest) {
     }
     return NextResponse.json({firmware}, {status: 200})
 }
+export async function POST(req: NextRequest, { params }: { params: { OLTID: string } }) {
+    // connectDB()
+    const { OLTID } = params
+    const firmwareFile = await req.json()
+    const firmware = await Firmware.findById(OLTID)
+    firmware.firmwareFile = firmwareFile
+    if(!firmware) {
+        return NextResponse.json({message: "No Firmware found"}, {status: 404})
+    }
+    
+    return NextResponse.json({OLTID, firmwareFile}, {status: 200})
+}
+
 
 export async function PUT(req: NextRequest, { params }: { params: { OLTID: string } }) {
     // connectDB()
