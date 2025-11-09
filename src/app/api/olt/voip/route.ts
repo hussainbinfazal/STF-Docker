73cdf11e14e6811b0c05ece465b97ec3a109f32d
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-10 01:20:13 +0530

    e-i

---
+++ src/app/api/olt/voip/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+export async function POST(request: NextRequest) : Promise<NextResponse> {
+    try {
+        const {number,Id,Password} = await request.json();
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+    }
+}
