0391da49a449716763678b8975c0515f106e9e36
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-11 16:30:26 +0530

    e-i updated

--- src/app/api/olt/voip/route.ts
+++ src/app/api/olt/voip/route.ts
@@ -10,3 +10,11 @@ export async function POST(request: NextRequest) : Promise<NextResponse> {
         return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
     }
 }
+export async function PUT(request: NextRequest) : Promise<NextResponse> {
+    try {
+        const {number,Id,Password} = await request.json();
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+    }
+}
