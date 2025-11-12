367f29aaadef5d44037b96fe002f053a5e51fa88
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-12 07:12:57 +0530

    e-i deleted

--- src/app/api/olt/voip/route.ts
+++ src/app/api/olt/voip/route.ts
@@ -18,3 +18,11 @@ export async function PUT(request: NextRequest) : Promise<NextResponse> {
         return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
     }
 }
+export async function DELETE(request: NextRequest) : Promise<NextResponse> {
+    try {
+        const {number,Id,Password} = await request.json();
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+    }
+}
