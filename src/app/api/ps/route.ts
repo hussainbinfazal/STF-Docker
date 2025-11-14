a0d0c673f763c454334c0a3d21d61fbd89ee06d1
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-14 07:02:24 +0530

    ps post controller added

---
+++ src/app/api/ps/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server"
+
+export async function POST(req: NextRequest, { params }: { params: { OLTID: string }}) {
+    connectDB()
+    try {
+        const { OLTID } = params
+        const firmware = await Firmware.findByIdAndDelete(OLTID)
+        if(!firmware) {
+            return NextResponse.json({message: "No Firmware found"}, {status: 404})
+        }
+    return NextResponse.json({OLTID}, {status: 200})
+    } catch (error: any) {
+        return NextResponse.json({message: error.message || "ERROR creating firmware"}, {status: 500})
+    }
+}
