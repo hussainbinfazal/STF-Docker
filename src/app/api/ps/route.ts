a6ebfa0786d508343f479af6b7618b8caa05cf48
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-15 07:34:34 +0530

    ps put controller added

--- src/app/api/ps/route.ts
+++ src/app/api/ps/route.ts
@@ -13,3 +13,16 @@ export async function POST(req: NextRequest, { params }: { params: { OLTID: stri
         return NextResponse.json({message: error.message || "ERROR creating firmware"}, {status: 500})
     }
 }
+export async function PUT(req: NextRequest, { params }: { params: { OLTID: string }}) {
+    connectDB()
+    try {
+        const { OLTID } = params
+        const firmware = await Firmware.findById(OLTID)
+        if(!firmware) {
+            return NextResponse.json({message: "No Firmware found"}, {status: 404})
+        }
+    return NextResponse.json({OLTID}, {status: 200})
+    } catch (error: any) {
+        return NextResponse.json({message: error.message || "ERROR creating firmware"}, {status: 500})
+    }
+}
