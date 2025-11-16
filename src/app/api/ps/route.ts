a851e181cf540825010601d1d27440d5558bbd96
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-16 07:22:44 +0530

    ps delete controller added

--- src/app/api/ps/route.ts
+++ src/app/api/ps/route.ts
@@ -26,3 +26,16 @@ export async function PUT(req: NextRequest, { params }: { params: { OLTID: strin
         return NextResponse.json({message: error.message || "ERROR creating firmware"}, {status: 500})
     }
 }
+export async function DELETE(req: NextRequest, { params }: { params: { OLTID: string }}) {
+    connectDB()
+    try {
+        const { OLTID } = params
+        const firmware = await Firmware.findByIdAndDelete(OLTID)
+        if(!firmware) {
+            return NextResponse.json({message: "No Firmware found"}, {status: 404})
+        }
+    return NextResponse.json({OLTID}, {status: 200})
+    } catch (error: any) {
+        return NextResponse.json({message: error.message || "ERROR creating firmware"}, {status: 500})
+    }
+}
