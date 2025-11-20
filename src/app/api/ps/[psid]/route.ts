ad11f1327637262f1f3d027e913bc5bd8abec95a
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-20 13:49:54 +0530

    psid update controller added

--- src/app/api/ps/[psid]/route.ts
+++ src/app/api/ps/[psid]/route.ts
@@ -12,6 +12,22 @@ export async function POST(req: NextRequest, { params }: { params: { psid: strin
         await PS.save()
         return NextResponse.json({ message: "PS Created Succesfully", PSID, ps }, { status: 200 })
     } catch (error: any) {
-        return NextResponse.json({ message: error.message || "ERROR creating firmware" }, { status: 500 })
+        return NextResponse.json({ message: error.message || "ERROR creating PS" }, { status: 500 })
+    }
+}
+export async function PUT(req: NextRequest, { params }: { params: { psid: string } }) {
+    connectDB()
+    try {
+        const { psid } = params
+        const body = await req.json()
+
+        let ps = await PS.findByIdAndUpdate(OLTID, body, { new: true })
+        if (ps) {
+            return NextResponse.json({ message: "PS already exists with this PSID" }, { status: 404 })
+        }
+        await PS.save()
+        return NextResponse.json({ message: "PS Created Succesfully", PSID, ps }, { status: 200 })
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message || "ERROR creating PS" }, { status: 500 })
     }
 }
