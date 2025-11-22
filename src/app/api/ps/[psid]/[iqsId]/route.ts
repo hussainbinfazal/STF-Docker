6577bc012bff3b5d5b92c383ebef67cc926f2c20
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-22 10:29:02 +0530

    qsid controller added

---
+++ src/app/api/ps/[psid]/[iqsId]/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest } from "next/server"
+
+export async function POST(req: NextRequest, { params }: { params: { psid: string } }) {
+    connectDB()
+    try {
+        const { qsid } = params
+        let ps = await PS.findById(qsid)
+        if (ps) {
+            return NextResponse.json({ message: "PS already exists with this PSID" }, { status: 404 })
+        }
+        ps = new PS({ id: qsid })
+        await PS.save()
+        return NextResponse.json({ message: "PS Created Succesfully", qsid, ps }, { status: 200 })
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message || "ERROR creating QS" }, { status: 500 })
+    }
+}
