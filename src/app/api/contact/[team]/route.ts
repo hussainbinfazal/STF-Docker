8a4612cf5a38def7e770c468daf223716565eb5f
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-27 00:25:53 +0530

    team controller added

---
+++ src/app/api/contact/[team]/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+
+export async function GET(req:NextResponse,context : {params: {team: string}}) {
+    connectDB()
+    const existingteamId = context.params.team;
+    const existingTeam = await Team.findOne({name: existingteam});
+
+    return NextResponse.json({team:existingTeam, message: "Team Request Updated"}, {status: 200})
+}
