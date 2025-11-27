12e8ccb83ef6fc28615b5c95d2a0ef31eee3349d
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-28 02:23:52 +0530

    team update controller added

--- src/app/api/contact/[team]/route.ts
+++ src/app/api/contact/[team]/route.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 
 
-export async function GET(req:NextResponse,context : {params: {team: string}}) {
+export async function GET(req: NextResponse, context: { params: { team: string } }) {
     connectDB()
     const existingteamId = context.params.team;
-    const existingTeam = await Team.findOne({name: existingteam});
+    const existingTeam = await Team.findOne({ name: existingteam });
 
-    return NextResponse.json({team:existingTeam, message: "Team Request Updated"}, {status: 200})
+    return NextResponse.json({ team: existingTeam, message: "Team Request Updated" }, { status: 200 })
+}
+export async function PUT(req: NextResponse, context: { params: { team: string } }) {
+    connectDB()
+    const existingteamId = context.params.team;
+    const body = request.json()
+    const existingTeam = await Team.findByIdAndUpdate({ id: existingteamId }, body, { new: true });
+
+    return NextResponse.json({ team: existingTeam, message: "Team Request Updated" }, { status: 200 })
 }
