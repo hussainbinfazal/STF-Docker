06387ce2e2227e447bd1e9aea8dc9d2b77ec73f6
hussainbinfazal <mdhussain0610@gmail.com>
2025-11-30 00:23:38 +0530

    team delete controller added

--- src/app/api/contact/[team]/route.ts
+++ src/app/api/contact/[team]/route.ts
@@ -16,3 +16,15 @@ export async function PUT(req: NextResponse, context: { params: { team: string }
 
     return NextResponse.json({ team: existingTeam, message: "Team Request Updated" }, { status: 200 })
 }
+export async function DELETE(req: NextResponse, context: { params: { team: string } }) {
+    try {
+        await connectDB()
+        const existingteamId = context.params.team;
+        const body = request.json()
+        const existingTeam = await Team.findByIdAndDelete({ id: existingteamId });
+
+        return NextResponse.json({ team: existingTeam, message: "Team with this ID Deleted Successfully" }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to delete team" }, { status: 500 })
+     }
+}
