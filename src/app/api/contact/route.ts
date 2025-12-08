ef4d2f3ac3baf8340ee1028ed481f507f6c5930a
hussainbinfazal <mdhussain0610@gmail.com>
2025-12-09 00:38:10 +0530

    type safety edited

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,49 +2,53 @@
 import { NextResponse } from "next/server";
 
 
-export async function GET(req:NextResponse) {
-    connectDB()
-    const Firmware = await Firmware.find()
-    if(!Firmware) {
-        return NextResponse.json({message: "No Firmware found"}, {status: 404})
-    }
-    return NextResponse.json({Firmware}, {status: 200})
-}
-
-export async function PUT(req:NextResponse,res:NextResponse) {
-    try{
+export async function GET(req: NextResponse) {
+    try {
         connectDB()
-    const Firmware = await Firmware.find()
-      
-    if(!Firmware) {
-        return NextResponse.json({message: "No Firmware found"}, {status: 404})
-    }
-    Firmware.fc = req.body
-    await Firmware.save()
-    return NextResponse.json({Firmware}, {status: 200})
-    }catch(error: any){
-        return NextResponse.json({message: "Internal Server Error"}, {status: 500})
-    }
-    finally{
-        return NextResponse.json({message: "Internal Server Error"}, {status: 500})
+        const Firmware = await Firmware.find()
+        if (!Firmware) {
+            return NextResponse.json({ message: "No Firmware found" }, { status: 404 })
+        }
+        return NextResponse.json({ Firmware }, { status: 200 })
+    } catch (error: any) {
 
     }
 }
-export async function DELETE(req:NextResponse,res:NextResponse) {
-    try{
-        const {firmware} = req.body
+
+export async function PUT(req: NextResponse, res: NextResponse) {
+    try {
         connectDB()
-     const FirmwareInDB = await Firmware.findByIdAndDelete(firmware)
-      
-    if(!FirmwareInDB) {
-        return NextResponse.json({message: "No Firmware found"}, {status: 404})
+        const Firmware = await Firmware.find()
+
+        if (!Firmware) {
+            return NextResponse.json({ message: "No Firmware found" }, { status: 404 })
+        }
+        Firmware.fc = req.body
+        await Firmware.save()
+        return NextResponse.json({ Firmware }, { status: 200 })
+    } catch (error: any) {
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 })
     }
-    return NextResponse.json({message : "Firmware Deleted Successfully"}, {status: 200})
-    }catch(error: any){
-        return NextResponse.json({message: "Internal Server Error , Please Try Again"}, {status: 500})
+    finally {
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 })
+
     }
-    finally{
-        return NextResponse.json({message: "Internal Server Error"}, {status: 500})
+}
+export async function DELETE(req: NextResponse, res: NextResponse) {
+    try {
+        const { firmware } = req.body
+        connectDB()
+        const FirmwareInDB = await Firmware.findByIdAndDelete(firmware)
+
+        if (!FirmwareInDB) {
+            return NextResponse.json({ message: "No Firmware found" }, { status: 404 })
+        }
+        return NextResponse.json({ message: "Firmware Deleted Successfully" }, { status: 200 })
+    } catch (error: any) {
+        return NextResponse.json({ message: "Internal Server Error , Please Try Again" }, { status: 500 })
+    }
+    finally {
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 })
 
     }
 }
