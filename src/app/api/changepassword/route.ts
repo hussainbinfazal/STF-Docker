f351ee65a77f34f9d5f14a47cd987e2f36b040a5
hussainbinfazal <mdhussain0610@gmail.com>
2025-12-13 11:50:51 +0530

    user types created & added

--- src/app/api/changepassword/route.ts
+++ src/app/api/changepassword/route.ts
@@ -6,47 +6,47 @@ import connectDB from "@/app/utils/db";
 export async function POST(req: NextRequest) {
     try {
         connectDB();
-    const { email, password } = await req.json();
-    const user = await User.findOne({ email });
-    if (!user) {
-        return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
-    user.password = password;
-    await user.save();
-    return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
+        const { email, password } = await req.json();
+        const user = await User.findOne({ email });
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+        user.password = password;
+        await user.save();
+        return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ message: error.message }, { status: 500 });
-        
+
     }
 }
 export async function PUT(req: NextRequest) {
     try {
         connectDB();
-    const { email, password } = await req.json();
-    const user = await User.findOne({ email });
-    if (!user) {
-        return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
-    user.password = password;
-    await user.save();
-    return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
+        const { email, password } = await req.json();
+        const user = await User.findOne({ email });
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+        user.password = password;
+        await user.save();
+        return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ message: error.message }, { status: 500 });
-        
+
     }
 }
 
 export async function DELETE(req: NextRequest) {
     try {
         connectDB();
-    const { userID} = await req.json();
-    const user = await User.findByIdAndUpdate(userID, { isActive: false });
-    if (!user) {
-        return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
-    return NextResponse.json({ message: "User Forgot successfull" }, { status: 200 });
+        const { userID } = await req.json();
+        const user: IUSER = await User.findByIdAndUpdate(userID, { isActive: false });
+        if (!user) {
+            return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+        return NextResponse.json({ message: "User Forgot successfull" }, { status: 200 });
     } catch (error: any) {
         return NextResponse.json({ message: error.message }, { status: 500 });
-        
+
     }
 }
