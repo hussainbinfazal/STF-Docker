4da6058417f836e0c53ff7d87451f634be2da1f6
hussainbinfazal <mdhussain0610@gmail.com>
2025-12-17 00:37:18 +0530

    team types created & added

--- src/app/api/contact/[team]/route.ts
+++ src/app/api/contact/[team]/route.ts
@@ -13,44 +13,50 @@ export async function GET(req: NextResponse, context: { params: { team: string }
     }
 }
 export async function PUT(req: NextResponse, context: { params: { team: string } }) {
-    connectDB()
-    const existingteamId = context.params.team;
-    const body = request.json()
-    const existingTeam = await Team.findByIdAndUpdate({ id: existingteamId }, body, { new: true });
-
-    return NextResponse.json({ team: existingTeam, message: "Team Request Updated" }, { status: 200 })
-}
-export async function DELETE(req: NextResponse, context: { params: { team: string } }) {
     try {
-        await connectDB()
-        const existingTeamId = context.params.team;
+        connectDB()
+        const existingteamId = context.params.team;
         const body = request.json()
-        const existingTeam : ITeam = await Team.findByIdAndDelete({ id: existingteamId });
+        const existingTeam:ITeam = await Team.findByIdAndUpdate({ id: existingteamId }, body, { new: true });
 
-        return NextResponse.json({ team: existingTeam, TeamId: existingTeamId, message: "Team with this ID Deleted Successfully" }, { status: 200 })
+        return NextResponse.json({ team: existingTeam, message: "Team Request Updated" }, { status: 200 })
+
     } catch (error: any) {
-        return NextResponse.json({ error: "Failed to delete team" }, { status: 500 })
+        return NextResponse.json({ error: "Failed to Update team" }, { status: 500 })
+
     }
-}
-export async function POSt(req: NextResponse, context: { params: { team: string } }) {
-    try {
 
-        await connectDB()
-        const body = await req.json()
-        if (!req.json().name || !req.json().role || !req.json().email) {
-            return NextResponse.json({ error: "Please fill all the fields" }, { status: 400 })
-        };
-        const newTeam:ITeam = new Team({
-            name: body.name,
-            role: body.role,
-            email: body.email,
-        });
-        await newTeam.save();
+    export async function DELETE(req: NextResponse, context: { params: { team: string } }) {
+        try {
+            await connectDB()
+            const existingTeamId = context.params.team;
+            const body = request.json()
+            const existingTeam: ITeam = await Team.findByIdAndDelete({ id: existingteamId });
 
-
-        return NextResponse.json({ team: newTeam, message: "New Team Created Successfully" }, { status: 200 })
-    } catch (error: any) {
-        const messsage = error.message || "Failed to create team";
-        return NextResponse.json({ error: "Failed to delete team" }, { status: 500 })
+            return NextResponse.json({ team: existingTeam, TeamId: existingTeamId, message: "Team with this ID Deleted Successfully" }, { status: 200 })
+        } catch (error: any) {
+            return NextResponse.json({ error: "Failed to delete team" }, { status: 500 })
+        }
     }
-}
+    export async function POSt(req: NextResponse, context: { params: { team: string } }) {
+        try {
+
+            await connectDB()
+            const body = await req.json()
+            if (!req.json().name || !req.json().role || !req.json().email) {
+                return NextResponse.json({ error: "Please fill all the fields" }, { status: 400 })
+            };
+            const newTeam: ITeam = new Team({
+                name: body.name,
+                role: body.role,
+                email: body.email,
+            });
+            await newTeam.save();
+
+
+            return NextResponse.json({ team: newTeam, message: "New Team Created Successfully" }, { status: 200 })
+        } catch (error: any) {
+            const messsage = error.message || "Failed to create team";
+            return NextResponse.json({ error: "Failed to delete team" }, { status: 500 })
+        }
+    }
