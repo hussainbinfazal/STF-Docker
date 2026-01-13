ca0bf7e5072051103dbecf1630eba82354c1526c
hussainbinfazal <mdhussain0610@gmail.com>
2026-01-13 20:23:59 +0530

    Types added

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(req: NextResponse) {
     try {
         connectDB()
-        const Firmware:IFirmware = await Firmware.find()
+        const Firmware:IFirmware | null = await Firmware.find()
         if (!Firmware) {
             return NextResponse.json({ message: "No Firmware found" }, { status: 404 })
         }
