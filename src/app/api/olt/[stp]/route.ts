90bed8578d98993954e58ba38d49255b4412ea47
hussainbinfazal <mdhussain0610@gmail.com>
2026-01-16 15:35:21 +0530

    Types added

--- src/app/api/olt/[stp]/route.ts
+++ src/app/api/olt/[stp]/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: NextRequest, { params }: { params: { stp: string
 export async function PUT(req: NextRequest, { params }: { params: { stp: string } }): Promise<NextResponse> {
     try {
         const { stp } = params
-        const stpInDb :IFirmware = await Firmware.findByIdAndUpdate(stp)
+        const stpInDb :IFirmware | null = await Firmware.findByIdAndUpdate(stp)
         return NextResponse.json({ message: "Not implemented", stp }, { status: 501 })
     } catch (error: any) {
         return NextResponse.json({ message: error.message || "ERROR processing request" }, { status: 500 })
