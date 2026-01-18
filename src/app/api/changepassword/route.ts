c3879ebbe206f9a70f8b0e2c78166c552d375d21
hussainbinfazal <mdhussain0610@gmail.com>
2026-01-18 16:41:13 +0530

    readme added

--- src/app/api/changepassword/route.ts
+++ src/app/api/changepassword/route.ts
@@ -23,7 +23,7 @@ export async function PUT(req: NextRequest) {
     try {
         connectDB();
         const { email, password } = await req.json();
-        const user:IUser = await User.findOne({ email });
+        const user:IUser | null = await User.findOne({ email });
         if (!user) {
             return NextResponse.json({ message: "User not found" }, { status: 404 });
         }
