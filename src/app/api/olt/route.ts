23d439a4197b164f75f275a61648dc293b24705f
hussainbinfazal <mdhussain0610@gmail.com>
2026-01-24 20:29:51 +0530

    user types added

--- src/app/api/olt/route.ts
+++ src/app/api/olt/route.ts
@@ -38,7 +38,7 @@ export async function PUT(req: NextRequest, { params }: { params: { OLTID: strin
 export async function DELETE(req: NextRequest, { params }: { params: { OLTID: string }}) {
     connectDB()
     const { OLTID } = params
-    const firmware = await Firmware.findByIdAndDelete(OLTID)
+    const firmware: Ifirmware = await Firmware.findByIdAndDelete(OLTID)
     if(!firmware) {
         return NextResponse.json({message: "No Firmware found"}, {status: 404})
     }
