af203e1286bc94c0b144e76179ecabe4c89c87b4
hussainbinfazal <mdhussain0610@gmail.com>
2026-02-18 00:08:54 +0530

    added comment for test in the dev

--- src/app/api/olt/[stp]/route.ts
+++ src/app/api/olt/[stp]/route.ts
@@ -30,7 +30,8 @@ export async function DELETE(req: NextRequest, { params }: { params: { stp: stri
 export async function GET(req: NextRequest, { params }: { params: { stp: string } }): Promise<NextResponse> {
     try {
         const { stp } = params
-        const stpInDb:IFirmware | null | UFirmware = await Firmware.findById(stp)
+        const stpInDb:IFirmware | null  = await Firmware.findById(stp) //Improve firmware types then update then make it firmware type
+         
         if(!stdInDb) return NextResponse.json({message: "Stp Not Found"}, {status:404})
 
         return NextResponse.json({ message: "STP", stpInDb }, { status: 201 })
