5fb575a71dcc28cd513ea3920d1f09045a30b108
hussainbinfazal <mdhussain0610@gmail.com>
2026-03-11 02:57:55 +0530

    added logger in the STP section for better logs in prod

--- src/app/api/olt/[stp]/route.ts
+++ src/app/api/olt/[stp]/route.ts
@@ -24,7 +24,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { stp: stri
     try {
         const { stp } = params
         const stpInDb:IFirmware | null  = await Firmware.findByIdAndDelete(stp)
-
+        logger.info("STP Deleted Successfully",{stpId: stp._id})
         return NextResponse.json({ message: "Stp Deleted Succesfully", stp }, { status: 501 })
     } catch (error: any) {
         return NextResponse.json({ message: error.message || "ERROR processing request" }, { status: 500 })
