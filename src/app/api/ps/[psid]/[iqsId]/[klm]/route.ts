8d823155e1007449699105053664578c735a6394
hussainbinfazal <mdhussain0610@gmail.com>
2026-03-26 00:10:26 +0530

    logger added in the KLM section

--- src/app/api/ps/[psid]/[iqsId]/[klm]/route.ts
+++ src/app/api/ps/[psid]/[iqsId]/[klm]/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest, { params }: { params: { psid: strin
         }
         ps = new PS({ id: klm })
         await PS.save()
-        logger.info("KLM crated Successfully")
+        logger.info("KLM crated Successfully",{KLMID:klm})
         return NextResponse.json({ message: "KLM Created Succesfully", klm, ps }, { status: 200 })
     } catch (error: any) {
         return NextResponse.json({ message: error.message || "ERROR creating KLM" }, { status: 500 })
