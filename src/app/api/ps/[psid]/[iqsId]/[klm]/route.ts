0f312ee6c3a9819e99ef5afc6b8050016c93c9cf
hussainbinfazal <mdhussain0610@gmail.com>
2026-05-09 00:31:36 +0530

    Update KLM route added

--- src/app/api/ps/[psid]/[iqsId]/[klm]/route.ts
+++ src/app/api/ps/[psid]/[iqsId]/[klm]/route.ts
@@ -35,3 +35,21 @@ export async function PUT(req: NextRequest, { params }: { params: { psid: string
         return NextResponse.json({ message: error.message || "ERROR creating KLM" }, { status: 500 })
     }
 }
+export async function DELETE(req: NextRequest, { params }: { params: { psid: string } }) {
+    connectDB()
+    try {
+        const { klm } = params
+        let ps:IPS | null = await PS.findById(klm)
+        if (ps) {
+            return NextResponse.json({ message: "KLM already exists with this KLMID" }, { status: 404 })
+        }
+        ps = new PS({ id: klm })
+        await PS.save()
+        logger.info("KLM crated Successfully",{KLMID:klm})
+        return NextResponse.json({ message: "KLM Created Succesfully", klm, ps }, { status: 200 })
+    } catch (error: any) {
+        const message = error instanceof Error ? error.message : "Internal Server Error"
+        logger.info("Error in post ps route",{message})
+        return NextResponse.json({ message: error.message || "ERROR creating KLM" }, { status: 500 })
+    }
+}
