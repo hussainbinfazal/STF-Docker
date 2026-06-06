f7087e7234b12db3794b1f90ee03d458c2ba22b4
hussainbinfazal <mdhussain0610@gmail.com>
2026-06-07 00:19:41 +0530

    error type refractored

--- src/app/api/ps/route.ts
+++ src/app/api/ps/route.ts
@@ -44,7 +44,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { OLTID: st
         }
         logger.info("Firmware Deleted Succesfully")
         return NextResponse.json({ message: "Your Old Firmware is Deleted ", OLTID }, { status: 200 })
-    } catch (error: any) {
+    } catch (error: unknown) {
         const message = error instanceof Error ? error.message: "Error in creating firmware"
         logger.error("Error in Deleting  Firmware")
         return NextResponse.json({ message: error.message || "ERROR creating firmware" }, { status: 500 })
