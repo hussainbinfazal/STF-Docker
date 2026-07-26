8e085c5117a3690b63d0146cd0e2f376e0e2cb61
hussainbinfazal <mdhussain0610@gmail.com>
2026-07-27 00:28:29 +0530

    Added Error Boundary

--- src/app/api/ps/[psid]/[iqsId]/route.ts
+++ src/app/api/ps/[psid]/[iqsId]/route.ts
@@ -3,7 +3,7 @@ export async function POST(req: NextRequest, { params }: { params: { psid: strin
     connectDB()
     try {
         const { qsid } = params
-        let ps:IPS |  null = await PS.findById(qsid)
+        let ps: IPS | null = await PS.findById(qsid)
         if (ps) {
             return NextResponse.json({ message: "PS already exists with this PSID" }, { status: 404 })
         }
@@ -12,24 +12,24 @@ export async function POST(req: NextRequest, { params }: { params: { psid: strin
         return NextResponse.json({ message: "PS Created Succesfully", qsid, ps }, { status: 200 })
     } catch (error: unknown) {
         const message = error instanceof Error ? error.message : "Something went wrong"
-        return NextResponse.json({ message: error.message || "ERROR creating QS" }, { status: 500 })
+        return NextResponse.json({ message: error.message || message || "ERROR creating QS" }, { status: 500 })
     }
 }
 export async function PUT(req: NextRequest, { params }: { params: { psid: string } }) {
     connectDB()
     try {
         const { qsid } = params
-        let ps:IPS | null = await PS.findById(qsid)
+        let ps: IPS | null = await PS.findById(qsid)
         if (ps) {
             return NextResponse.json({ message: "PS already exists with this PSID" }, { status: 404 })
         }
         ps = new PS({ id: qsid })
         await PS.save()
-        logger.info("PS Updated Successfully",{qsid})
+        logger.info("PS Updated Successfully", { qsid })
         return NextResponse.json({ message: "PS Updated Succesfully", qsid, ps }, { status: 200 })
     } catch (error: unknown) {
         const message = error instanceof Error ? error.message : "Internal server Error"
-        logger.error("Error in update route controller Ps",{
+        logger.error("Error in update route controller Ps", {
             message
         })
         return NextResponse.json({ message: error.message || "ERROR updating QS with this ID" }, { status: 500 })
@@ -39,12 +39,12 @@ export async function DELETE(req: NextRequest, { params }: { params: { psid: str
     connectDB()
     try {
         const { qsid } = params
-        let ps:IPS | null = await PS.findByIdAndDelete(qsid)
+        let ps: IPS | null = await PS.findByIdAndDelete(qsid)
         logger.info("Qs Deleted Successfully")
         return NextResponse.json({ message: "QS Deleted  Succesfully", qsid, ps }, { status: 200 })
     } catch (error: unknown) {
         const message = error instanceof Error ? error.message : "Internal server Error"
-        logger.info("Error In Deleting the qsId server side",{
+        logger.info("Error In Deleting the qsId server side", {
             message
         }
         )
